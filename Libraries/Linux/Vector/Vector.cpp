c3b8709ea8ddc67ae34f6b71a90d01bbdaa83c71
ShabuniaPavel <shabuniap@gmail.com>
2026-01-03 18:07:34 +0300

    Add file

---
+++ Libraries/Linux/Vector/Vector.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+
+#include "Vector.h"
+#include <cmath>
+#include <iostream>
+
+Vector::Vector(Number xx, Number yy) : x(xx), y(yy) {}
+
+Number Vector::GetX() const { return x; }
+Number Vector::GetY() const { return y; }
+
+Number Vector::GetRadius() const {
+    double r = std::sqrt(x.GetValue() * x.GetValue() + y.GetValue() * y.GetValue());
+    return Number::Create(r);
+}
+
+Number Vector::GetAngle() const {
+    double angle = std::atan2(y.GetValue(), x.GetValue());
+    return Number::Create(angle);
+}
+
+Vector Vector::Add(const Vector& other) const {
+    return Vector(x + other.x, y + other.y);
+}
+
+void Vector::Print() const {
+    std::cout << "(" << x.GetValue() << ", " << y.GetValue() << ")"
+        << " r=" << GetRadius().GetValue()
+        << " phi=" << GetAngle().GetValue() << " rad\n";
+}
+
+const Vector Vector::ZeroVector(Number::Zero, Number::Zero);
+const Vector Vector::OneOneVector(Number::One, Number::One);
---
+++ Libraries/Linux/Vector/Vector.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "Number.h"
+
+class Vector {  
+private:
+    Number x;
+    Number y;
+
+public:
+    Vector(Number xx = Number::Zero, Number yy = Number::Zero);
+
+    Number GetX() const;
+    Number GetY() const;
+
+    Number GetRadius() const;
+    Number GetAngle() const;
+
+    Vector Add(const Vector& other) const;
+
+    void Print() const;
+
+    static const Vector ZeroVector;
+    static const Vector OneOneVector;
+};
