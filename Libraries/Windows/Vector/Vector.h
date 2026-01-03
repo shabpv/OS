f471fe16610de6eeef5c0107769da0d817435eb7
ShabuniaPavel <shabuniap@gmail.com>
2026-01-03 04:15:04 +0300

    Add file

---
+++ Libraries/Windows/Vector/Vector.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "Number.h"
+
+#ifdef VECTOR_EXPORTS
+#define VECTOR_API __declspec(dllexport)
+#else
+#define VECTOR_API __declspec(dllimport)
+#endif
+
+class VECTOR_API Vector {
+private:
+    Number x;
+    Number y;
+
+public:
+    Vector(Number xx = Number::Zero, Number yy = Number::Zero);
+
+    Number GetX() const;
+    Number GetY() const;
+
+    Number GetRadius() const;
+    Number GetAngle() const;
+
+    Vector Add(const Vector& other) const;
+
+    void Print() const;
+
+    static const Vector ZeroVector;
+    static const Vector OneOneVector;
+};
